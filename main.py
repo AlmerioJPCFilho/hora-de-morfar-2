ab7b1b22f53368283646d741e631799ede6fa12e
adcp-max <adcp@cin.ufpe.br>
2025-12-18 03:06:41 -0300

    Commit adcp

--- main.py
+++ main.py
@@ -128,6 +128,17 @@ txt_sair = fonte.render(msg_sair, False, (130, 0, 0))
 telainicial = pygame.image.load(os.path.join(diretorio_imagens, 'tela-inicial.png')).convert()
 transformarTelaInicial = pygame.transform.scale(telainicial, (1280, 720))
 
+# TEMPORIZADOR
+fonte = pygame.font.SysFont(None, 50)
+#FUNÇÂO TEMPO
+def timer(tempo):
+    lista = str(tempo)
+    if len(lista) >= 4:
+        return lista[0]+lista[1]
+    elif len(lista) < 3:
+        return "0."+lista[0]
+    return lista[0]
+
 def iniciar_jogo():
     global musica_titulo_rodando
     retanguloTransparente = pygame.Surface((320, 60))
@@ -199,6 +210,7 @@ def gameOver(tela, transformartelademorte):
 
 def jogo():
     global pontos_carteira, pontos_cerveja, pontos_detergente, pontos_policial, jogadorMorreu, som_coleta1, som_coleta2, som_coleta3, musica_jogo_rodando, musica_titulo, musica_titulo_rodando
+    tempo_restante -= 1100
     while True:
         if jogadorMorreu:
             gameOver(tela, transformartelademorte)
@@ -212,6 +224,10 @@ def jogo():
         tela.fill(preto)
         tela.blit(background, (0, 0))
         todas_sprites.draw(tela)
+        tempo_restante -= 1.5
+        tempo_print = timer(int(tempo_restante))
+        mensagem = fonte.render(f"Tempo Restante: {tempo_print}", False, (255, 255, 255))
+        tela.blit(mensagem, (500, 20))
 
         msg_carteira = f': {pontos_carteira}'
         txt_carteira = fonte.render(msg_carteira, False, (255, 255, 255))
@@ -297,7 +313,7 @@ def jogo():
         tela.blit(txt_policial, (largura_tela - 45, 20))
 
         todas_sprites.update()
-        if pontos_detergente > 10:
+        if tempo_restante <= 0:
             jogadorMorreu = True
         pygame.display.flip()
 
